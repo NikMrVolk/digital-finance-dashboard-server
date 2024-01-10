22035f3c525527837e49e87013f62947fd6c0415
NickZavadski <nick.mr.volk@gmail.com>
2024-01-10 16:59:50 +0300

    feat: work with server cookies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    imports: [ConfigModule.forRoot(), AuthModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5005
+        const app = await NestFactory.create(AppModule)
+
+        app.setGlobalPrefix('api')
+        app.use(cookieParser())
+        app.enableCors({
+            origin: ['http://localhost:3000'],
+            credentials: true,
+            // exposedHeaders: 'set-cookie',
+        })
+
+        await app.listen(PORT, () =>
+            console.log(`App started work on port ${PORT}`),
+        )
+    } catch (e) {
+        console.log(e)
+    }
 }
-bootstrap();
+
+start()
